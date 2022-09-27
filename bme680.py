4ea380081f0c84b8e08f3c5bfb391d606f0b44b9
octaprog7 <goctaprog@gmail.com>
2022-09-28 01:20:05 +0500

    Initial commit

---
+++ bme680.py
@@ -0,0 +1,217 @@
+import micropython
+
+from sensor_pack import bus_service, base_sensor
+from sensor_pack.base_sensor import Device, Iterator
+from sensor_pack import bitfield
+
+
+class BME680bosh(Device, Iterator):
+    """Class for work with Bosh BME680 sensor"""
+    def __init__(self, adapter: bus_service.BusAdapter, address=0x77):
+        super().__init__(adapter, address, False)
+        # osrs_id = 0     относительная влажность
+        # osrs_id = 1     давление
+        # osrs_id = 2     температура
+        self.osrs = bytearray(3)
+        self.IIR_filter = 0
+        self.mode = False
+        # self.enable_gas_conversion = True
+        self._i2c_mode = True
+
+    @staticmethod
+    def _get_raw_wt(val: int) -> tuple:
+        """
+        :param val: 0..4094 [ms] wait time for gas sensor setpoint
+        :return: tuple(multiplier, wait_time)
+        """
+        i, v = 0, 0
+        for i in range(4):
+            v = val // 4 ** i
+            if v <= 64:
+                break
+        return 4 ** i, v
+
+    @staticmethod
+    def _check_gas_id(id: int) -> int:
+        return base_sensor.check_value(id, range(10), f"Invalid id value: {id}")
+
+        # BaseSensor
+    def _read_register(self, reg_addr, bytes_count=2) -> bytes:
+        """считывает из регистра датчика значение.
+        bytes_count - размер значения в байтах"""
+        return self.adapter.read_register(self.address, reg_addr, bytes_count)
+
+        # BaseSensor
+    def _write_register(self, reg_addr, value: [int, bytes, bytearray], bytes_count=2) -> int:
+        """записывает данные value в датчик, по адресу reg_addr.
+        bytes_count - кол-во записываемых данных"""
+        byte_order = self._get_byteorder_as_str()[0]
+        return self.adapter.write_register(self.address, reg_addr, value, bytes_count, byte_order)
+
+    def set_mode(self, forced_mode: bool = True):
+        """Управление режимом работы датчика.
+        Если forced_mode Истина, то датчик переходит в режим Forced mode,
+        иначе датчик переходит в режим сна (Sleep mode).
+        Sensor operation mode control.
+        If forced_mode is True, then the sensor goes into Forced mode,
+        otherwise the sensor goes into sleep mode (Sleep mode)"""
+        addr, lshift = 0x74, 0
+        bf = bitfield.BitField(alias=None, start=lshift, stop=1 + lshift)
+        val = self._read_register(addr, 1)[0]
+        self._write_register(0x74, bf.put(val, int(forced_mode)), 1)
+        self.mode = forced_mode
+
+    def set_oversampling(self, osrs_id: int, osrs_value: int = 2):
+        """Устанавливает значение передискретизации для:
+        osrs_id = 0     относительная влажность
+        osrs_id = 1     давление
+        osrs_id = 2     температура
+
+        Sets the oversampling value for:
+         osrs_id = 0 relative humidity
+         osrs_id = 1 pressure
+         osrs_id = 2 temperature
+        """
+        base_sensor.check_value(osrs_id, range(3), f"Invalid osrs_id value: {osrs_id}")
+        base_sensor.check_value(osrs_value, range(6), f"id: {osrs_id}. Invalid osrs_value: {osrs_value}")
+        # (address, left shift count)
+        reg_dat = ((0x72, 0), (0x74, 2), (0x74, 5))
+        addr, lshift = reg_dat[osrs_id]
+        val = self._read_register(addr, 1)[0]
+        bf = bitfield.BitField(start=lshift, stop=2 + lshift, alias=None)
+        self._write_register(addr, bf.put(val, osrs_value), 1)
+        self.osrs[osrs_id] = osrs_value
+
+    def get_oversampling(self, osrs_id: int) -> int:
+        """Return oversampling value.
+        osrs_id = 0     relative humidity
+        osrs_id = 1     atmosphere pressure
+        osrs_id = 2     temperature"""
+        base_sensor.check_value(osrs_id, range(3), f"Invalid osrs_id value: {osrs_id}")
+        # (address, left shift count)
+        reg_dat = ((0x72, 0), (0x74, 2), (0x74, 5))
+        addr, lshift = reg_dat[osrs_id]
+        x = self._read_register(addr, 1)[0]
+        bf = bitfield.BitField(start=lshift, stop=2 + lshift, alias=None)
+        return bf.get(x)
+
+    def set_iir_filter(self, iir_value: int = 2):
+        """
+        IIR-фильтр применяется к данным о температуре и давлении, но не к данным о влажности и газе!
+        Данные, поступающие от АЦП, фильтруются, а затем загружаются в регистры данных.
+        Регистры результатов температуры и давления обновляются одновременно в конце измерения.
+
+        IIR filter applies to temperature and pressure data but not to humidity and gas data.
+        The data coming from the ADC are filtered and then loaded into the data registers.
+        The temperature and pressure result registers are updated together at the same time at the end
+        of the measurement."""
+        base_sensor.check_value(iir_value, range(8), f"Invalid iir_value value: {iir_value}")
+        addr, lshift = 0x75, 2
+        val = self._read_register(addr, 1)[0]
+        bf = bitfield.BitField(alias=None, start=lshift, stop=2 + lshift)
+        self._write_register(addr, bf.put(val, iir_value), 1)
+        self.IIR_filter = iir_value
+
+    def get_id(self):
+        """Возвращает идентификатор датчика и его revision ID.
+        Правильное значение: 0x61
+        Returns the ID and revision ID of the sensor."""
+        return self._read_register(0xD0 if self._i2c_mode else 0x50, 1)[0]
+
+    def soft_reset(self):
+        """Software reset"""
+        self._write_register(0xE0 if self._i2c_mode else 0x60, 0xB6, 1)
+
+    # GAS
+    def heater_set_current(self, id: int, current: int):
+        """
+        Setup sensor gas heater with current
+        :param current: 0..15 [mA]
+        :param id: 0..9
+        :return: None
+        """
+        BME680bosh._check_gas_id(id)
+        base_sensor.check_value(current, range(17), f"Invalid current value: {current}")
+        raw_curr = (current << 3) - 1
+        self._write_register(0x50 + id, raw_curr << 1, 1)   # bit 7..1 used, see documentation
+
+    def heater_set_resistance(self, id: int, value: int):
+        """
+        Setup sensor gas heater with resistance
+        :param id: 0..9
+        :param value: Decimal value that needs to be stored for achieving target heater resistance
+        :return: None
+        """
+        BME680bosh._check_gas_id(id)
+        base_sensor.check_value(value, range(0x100), f"Invalid current value: {value}")
+        self._write_register(0x5A + id, value, 1)
+
+    def heater_set_wait_time(self, id: int, wait_time: int):
+        """
+        Setup sensor gas heater with wait_time
+        :param id: 0..9
+        :param wait_time: 0..4096 [ms]
+        :return: wait time in [ms]
+        """
+        BME680bosh._check_gas_id(id)
+        base_sensor.check_value(wait_time, range(4097), f"Invalid id value: {id}")
+        t = BME680bosh._get_raw_wt(wait_time)
+        self._write_register(0x64 + id, (t[0] << 6) | t[1], 1)
+
+    def heater_setpoint(self, id: int, current: int, resistance: int, wait_time: int):
+        """
+        Setup sensor gas heater with current, resistance, wait_time
+        :param id: 0..9
+        :param current: 0..15 [mA]
+        :param resistance: Decimal value that needs to be stored for achieving target heater resistance
+        :param wait_time: 0..4096 [ms]
+        :return:
+        """
+        self.heater_set_current(id, current)
+        self.heater_set_resistance(id, resistance)
+        self.heater_set_wait_time(id, wait_time)
+
+    def heater_enable_setpoint(self, id: int, run_gas_conversion: bool):
+        BME680bosh._check_gas_id(id)
+        self._write_register(0x71, (int(run_gas_conversion) << 4) | id, 1)
+
+    def heater_on(self, on: bool):
+        """Turn on/off current injected to heater"""
+        self._write_register(0x70, (int(not on) << 3), 1)
+
+    # Data registers
+    def _get_3x_data(self, start_addr: int) -> int:
+        msb, lsb, xlsb = self._read_register(start_addr, 3)
+        return (msb << 12) | (lsb << 4) | (xlsb & 0xF0) >> 4
+
+    def get_pressure(self) -> int:
+        """return raw pressure"""
+        return self._get_3x_data(0x1F)
+
+    def get_temperature(self) -> int:
+        """return raw pressure"""
+        return self._get_3x_data(0x22)
+
+    def get_humidity(self) -> int:
+        """return raw humidity"""
+        b = self._read_register(0x25, 2)
+        return self.unpack("H", b)[0]
+
+    def get_gas_resistance_data(self) -> tuple:
+        """Return (range_of_measured_gas_sensor_resistance, gas_sensor_resistance_data)"""
+        msb, lsb = self._read_register(0x2A, 2)
+        return lsb & 0x0F, (msb << 2) | ((lsb & 0xC0) >> 6)
+
+    def get_gas_meas_status(self) -> tuple:
+        ... 0x1D
+
+    def get_gas_valid_status(self) -> tuple:
+        ...0x2B
+
+
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        ...
---
+++ main.py
@@ -0,0 +1,22 @@
+from machine import I2C, Pin
+import bme680
+import time
+from sensor_pack.bus_service import I2cAdapter
+
+
+if __name__ == '__main__':
+    # пожалуйста установите выводы scl и sda в конструкторе для вашей платы, иначе ничего не заработает!
+    # please set scl and sda pins for your board, otherwise nothing will work!
+    # https://docs.micropython.org/en/latest/library/machine.I2C.html#machine-i2c
+    # i2c = I2C(0, scl=Pin(13), sda=Pin(12), freq=400_000) № для примера
+    # bus =  I2C(scl=Pin(4), sda=Pin(5), freq=100000)   # на esp8266    !
+    # Внимание!!!
+    # Замените id=1 на id=0, если пользуетесь первым портом I2C !!!
+    # Warning!!!
+    # Replace id=1 with id=0 if you are using the first I2C port !!!
+    i2c = I2C(id=1, freq=400_000)  # on Arduino Nano RP2040 Connect tested
+    adaptor = I2cAdapter(i2c)
+    # bme - sensor
+    bme = bme680.BME680bosh(adaptor)
+    chip_id = bme.get_id()
+    print(f"chip_id: {hex(chip_id)}")
